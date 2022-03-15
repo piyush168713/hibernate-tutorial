6838371eaf343114dc1e624b0f8bf404ee53b7ea
Piyush Kumar <20BCS9107@cuchd.in>
2022-03-15 10:43:35 +0530

    Testing setup

---
+++ src/com/luv2code/jdbc/TestJdbc.java
@@ -0,0 +1,24 @@
+package com.luv2code.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestJdbc {
+    public static void main(String[] args){
+
+        String jdbcUrl = "jdbc:mysql://localhost:3306/hb_student_tracker?allowPublicKeyRetrieval=true&useSSL=false";
+        String user = "hbstudent";
+        String pass = "hbstudent";
+
+        try {
+            System.out.println("Connecting to database: " + jdbcUrl);
+
+            Connection myConn = DriverManager.getConnection(jdbcUrl, user, pass);
+
+            System.out.println("Connection Successful!!!");
+        }
+        catch (Exception exc){
+            exc.printStackTrace();
+        }
+    }
+}
